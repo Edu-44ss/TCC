392c7858a32a337a091b8093dce73c99dc4c3b27
Edu-44ss <Edagraca755@gmail.com>
2024-09-15 22:48:39 -0300

    Add files via upload

--- Projeto-Site-TCC/src/Sql/Gerações.sql
+++ Projeto-Site-TCC/src/Sql/Gerações.sql
@@ -1,28 +1,37 @@
-create database Gerações;
-use Gerações;
+create database gerações;
+use gerações;
 
-create table Cliente (
-id_cliente int not null auto_increment primary key,
-Nome varchar(60),
-RG varchar(13),
-CPF varchar(15),
-Endereco varchar(30),
-Telefone varchar(15)
+create table cursos(
+id_curso int not null auto_increment primary key,
+nome_curso varchar(20),
+categoria_curso varchar(20),
+descrição_curso text,
+valor_curso decimal(5,2)
 );
 
-desc Cliente;
-select * from Cliente;
-insert into Cliente(Nome, RG, CPF, Endereco, Telefone)values('Renato Vinícius Gomes','15.171.154-5','137.260.079-54','Rua José Cadilhe','41 997015919'),
-('Isaac Rosa de Jesus','15.171.154-5','137.260.079-54','Rua José Cadilhe','41 997015919'),
-('Eduardo da Graça Firmino','15.171.154-5','137.260.079-54','Rua José Cadilhe','41 997015919');
+create table registro(
+id_login int not null auto_increment primary key,
+email varchar(30),
+nick varchar(15), /* nome ficticio e publico para o perfil dos usuarios*/
+senha varchar(15),
+cod_curso int,
+foreign key (cod_curso) references cursos(id_curso)
+);
 
+create table instituição (
+id_instituição int not null auto_increment primary key,
+nome_professores varchar(30),
+staff varchar(20),
+num_alunos varchar(20),
+num_professor varchar(20),
+descrição text
+);
 
-create table Escola(
-id_escola int not null auto_increment primary key,
-Nome varchar(16),
-Endereco varchar(30),
-Telefone varchar(15),
-CNPJ varchar(60),
-cod_cliente int,
-foreign key (cod_cliente) references Cliente(id_cliente)
+create table pedido (
+id_pedido int not null auto_increment primary key,
+nome_curso varchar(20),
+cod_login int,
+cod_curso int,
+foreign key (cod_login) references registro(id_login),
+foreign key (cod_curso) references cursos(id_curso)
 );
